095f70fe90d0a8f363cd1ae9fe10aea79db74e6c
yogainstructorpauls <elliott.lidsl@gmail.com>
2026-08-28 11:15:26 +0300

    a

---
+++ part1/src/App.jsx
@@ -0,0 +1,45 @@
+const Header = (props) => {
+  console.log(props)
+  return <h1>{props.course}</h1>
+}
+
+const Part = (props) => {
+  return (
+    <p>{props.part.name} {props.part.exercises}</p>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.parts[0]}/>
+      <Part part={props.parts[1]}/>
+      <Part part={props.parts[2]}/>
+      </div>
+  )
+}
+
+const Total = (props) => {
+  return (<p>{props.parts[0] + props.parts[1] + props.parts[2]}</p>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      { name: 'Fundamentals of React', exercises: 10},
+      { name: 'Using props to pass data', exercises: 7},
+      { name: 'State of a component', exercises: 14}
+    ]
+  }
+  return (
+    <div>
+      <Header course={course.name}/>
+      <Content parts={course.parts}/>
+      <Total parts={course.parts}/>
+    </div>
+  )
+}
+
+export default App
