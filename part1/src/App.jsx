5e4796ac04309e277f54450a15a63bf568574bcc
yogainstructorpauls <elliott.lidsl@gmail.com>
2026-09-11 10:54:16 +0300

    Complete exercise 1.6

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -20,7 +20,7 @@ const Content = (props) => {
 }
 
 const Total = (props) => {
-  return (<p>{props.parts[0] + props.parts[1] + props.parts[2]}</p>
+  return (<p>{props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
   )
 }
 
---
+++ part1/src/App16.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+        <h1>Give Feedback</h1>
+      <button onClick={() => setGood(good + 1)}>Good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
+      <button onClick={() => setBad(bad + 1)}>Bad</button>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral} </p>
+      <p>Bad: {bad}</p>
+    </div>
+  )
+}   
+
+export default App
